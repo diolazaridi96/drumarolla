2297a7479a4aa7c227732e27d8e46cf26513c2de
diolazaridi96 <diolazaridi96@gmail.com>
2025-11-16 00:43:03 +0400

    Update app.py

--- spleeter/app.py
+++ spleeter/app.py
@@ -54,5 +54,5 @@ def separate_audio():
 
 if __name__ == "__main__":
     # Используем порт, который предоставляет Railway
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 6000))
     app.run(host="0.0.0.0", port=port)
